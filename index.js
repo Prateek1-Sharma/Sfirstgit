e3ab1ec398b6c723070c33a8fd052018eaa17dde
Prateek1-Sharma <prateeksharma222000@gmail.com>
2024-03-02 21:29:08 +0530

    new branch

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const mainHead=document.getElementById("main-heading");
-mainHead.textContent='Fruit World';
-mainHead.style.color='orange';
-const header=document.getElementById("header");
-header.style.borderBottom="2px solid orange";
-header.style.background="green";
-const basketheading=document.getElementById("basket-heading");
-basketheading.style.color='green';
-const thanks=document.getElementById("thanks");
-thanks.innerHTML="<p>Please visit us again</p>";
+const fruits = document.getElementsByClassName("fruit");
+fruits[2].style.background = "yellow";
+for(let i=0;i<fruits.length;i++)
+  {
+    fruits[i].style.fontWeight="bold";
+  }
