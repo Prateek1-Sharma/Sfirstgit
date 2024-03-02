208db11057389ca83727dd6fc43b73f949fe31b8
Prateek1-Sharma <prateeksharma222000@gmail.com>
2024-03-02 20:41:14 +0530

    html and js file added

--- app.js
+++
@@ -1,2 +0,0 @@
-console.log("i Love Git");
-console.log("Vishal is my Friend");
---
+++ index.js
@@ -0,0 +1,10 @@
+const mainHead=document.getElementById("main-heading");
+mainHead.textContent='Fruit World';
+mainHead.style.color='orange';
+const header=document.getElementById("header");
+header.style.borderBottom="2px solid orange";
+header.style.background="green";
+const basketheading=document.getElementById("basket-heading");
+basketheading.style.color='green';
+const thanks=document.getElementById("thanks");
+thanks.innerHTML="<p>Please visit us again</p>";
