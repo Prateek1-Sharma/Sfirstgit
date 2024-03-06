d1e652e84848a25ffa871fec12bc1e9077b6fde6
Prateek1-Sharma <prateeksharma222000@gmail.com>
2024-03-07 00:54:03 +0530

    stringify method

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-he Edit Button:
-const ul=document.querySelector("ul");
-const li=document.querySelectorAll("li");
-for(let i=0;i<li.length;i++)
-{
-const editButton=document.createElement("button");
-const editbuttontext=document.createTextNode("Edit");
-editButton.className="edit-btn";
-li[i].appendChild(editButton);
-}
-// Implement the code as in video but with one extra 'Edit' button in 'li'
 function handleFormSubmit(event)
 {
-  event.preventDefault();
+event.preventDefault();
 const name=event.target.username.value;
 const email=event.target.email.value;
 const phone=event.target.phone.value;
-  localStorage.setItem("Username",name);
-  localStorage.setItem("Email",email)
-  localStorage.setItem("Phone",phone);
+const userDetails= {
+  username:name,
+  email:email,
+  phone:phone
 }
-const form=document.querySelector("form");
-form.addEventListener("submit", handleFormSubmit)
-module.exports=handleFormSubmit
+ const userDetail = JSON.stringify(userDetails);
+  localStorage.setItem("User Details",userDetail)
+}
+
+module.exports=handleFormSubmit;
