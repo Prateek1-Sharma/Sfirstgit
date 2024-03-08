7c067dbb99a4a669243da716d4f46a31bdf677ae
Prateek1-Sharma <prateeksharma222000@gmail.com>
2024-03-08 16:19:23 +0530

    edit functionality added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Write your code below:
 
 const ul=document.querySelector("ul");
-
+const popname=document.querySelector("#username");
+const popemail=document.querySelector("#email");
+const popphone=document.querySelector("#phone");
 
 
 function handleFormSubmit(event){
@@ -15,18 +17,33 @@ const phone=event.target.phone.value;
     phone:phone
   }
   const userDetail=JSON.stringify(userDetails);
-  localStorage.setItem("User Details",userDetail)
+  localStorage.setItem(email,userDetail)
   const li=document.createElement("li");
   const litext=document.createTextNode(name+" "+email+" "+phone);
   const button2=document.createElement("button");
   button2.setAttribute("id", "newId");
   button2.textContent="delete";
+  const edit=document.createElement("button");
+  edit.setAttribute("id", "new");
+  edit.textContent="Edit";
   li.appendChild(litext);
   li.appendChild(button2);
+  li.appendChild(edit);
   ul.append(li);
   button2.addEventListener("click", function(event){
-    this.parentNode.remove(); 
-    localStorage.removeItem("User Details");
+  this.parentNode.remove(); 
+  localStorage.removeItem(email);
+    
+})
+edit.addEventListener("click", function(event){
+  localStorage.removeItem(email);
+  popname.value=name;
+  popemail.value=email;
+  popphone.value=phone;
+
+
+
+  this.parentNode.remove(); 
     
 })
 }
