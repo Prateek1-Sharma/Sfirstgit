511e2e61f70f7a7989abfa127ece5d9a7702255c
Prateek1-Sharma <prateeksharma222000@gmail.com>
2024-03-08 15:13:55 +0530

    del functionality

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-function handleFormSubmit(event)
-{
+// Write your code below:
+
+const ul=document.querySelector("ul");
+
+
+
+function handleFormSubmit(event){
 event.preventDefault();
 const name=event.target.username.value;
 const email=event.target.email.value;
 const phone=event.target.phone.value;
-const userDetails= {
-  username:name,
-  email:email,
-  phone:phone
-}
- const userDetail = JSON.stringify(userDetails);
+  const userDetails={
+    name:name,
+    email:email,
+    phone:phone
+  }
+  const userDetail=JSON.stringify(userDetails);
   localStorage.setItem("User Details",userDetail)
+  const li=document.createElement("li");
+  const litext=document.createTextNode(name+" "+email+" "+phone);
+  const button2=document.createElement("button");
+  button2.setAttribute("id", "newId");
+  button2.textContent="delete";
+  li.appendChild(litext);
+  li.appendChild(button2);
+  ul.append(li);
+  button2.addEventListener("click", function(event){
+    this.parentNode.remove(); 
+    localStorage.removeItem("User Details");
+    
+})
 }
 
-module.exports=handleFormSubmit;
+   
+
